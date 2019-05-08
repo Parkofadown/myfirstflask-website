9b4250dd3a93c1bd325d0c012115b3b20261641c
Richard <38399137+Parkofadown@users.noreply.github.com>
2019-05-08 17:31:38 -0400

    Add files via upload

--- firstflasksite/index.py
+++ firstflasksite/index.py
@@ -3,25 +3,25 @@ from flask import request
 from flask import render_template
 app = Flask(__name__)
 # Basic Routing
-@app.route('/')#127.0.0.1:5000
+@app.route('/')  # 127.0.0.1:5000
 def index():
-    return render_template('bootstrap template.html')
-#html files inside templates
-#images go into static
-#@app.route('/puppy_latin/<name>')#127.0.0.1:5000/information
-#def puppylatin(name):
+    return render_template('index.html')
 
-    #pupname = ''
-    #if name[-1] == 'y':
-    #    pupname = name[:-1] + 'iful'
-    #else:
-        #pupname = name + 'y'
-    #return "<h1> Your puppy latin name is: {}".format(pupname)
 
-# Dynamic Routing
-#@app.route('/puppy/<name>')
-#def puppy(name):
-#    return "<h1> This is a page for {} </h1>".format(name)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/photos')
+def photos():
+    return render_template('photos.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
